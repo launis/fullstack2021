ecb5d83767a6dd89d2b2e66523664f02bd54d70b
launis <62068629+launis@users.noreply.github.com>
2021-12-09 04:58:37 +0200

    Basics  2.17

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect  } from 'react'
 
 import Header from "./Components/Header"
 import PersonForm from "./Components/PersonForm"
 import Filter from "./Components/Filter"
 import Persons from "./Components/Persons"
+import personService from "./Services/PersonsServices"
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(persons => {
+        setPersons(persons)
+      })
+  }, [])
+
   const [newSearch, setNewSearch] = useState("")
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -24,6 +28,18 @@ const App = () => {
   const handleNameChange = (event) => {setNewName(event.target.value)}
   const handleNumberChange = (event) => {setNewNumber(event.target.value)}
 
+
+  const toggleDelOf = ({ person }) => {
+    console.log(`importance of ${person} needs to be toggled`)
+    if (window.confirm(`Are you sure you want to delete ${person.name}?`)) {
+      personService
+        .del(person.id)
+        .then(returnedPerson => {
+          setPersons(persons.filter((p) => p.id !== person.id))
+        })}
+  }
+
+
   const addPerson = event => {
     event.preventDefault()
 
@@ -33,21 +49,24 @@ const App = () => {
       {window.alert(`${newName} is already added to phonebook`)}
     else {
       
-      setPersons(persons.concat(personObject))}
-      axios
-      .post('http://localhost:3001/notes', personObject)
-      .then(response => {
-        setNewName(persons.concat(response.data))
-        setNewNote('')
+    setPersons(persons.concat(personObject))}
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
       })
   }
-
 
 
 
 return (
     <>
       <Header header={'Phonebook'} />
+
+
+
 
 
       <Filter
@@ -69,7 +88,12 @@ return (
 
       <Header header={'Numbers'} />
 
-      <Persons persons={persons} newSearch={newSearch} />
+      <Persons
+        persons={persons} 
+        newSearch={newSearch}
+        toggleDel={toggleDelOf}
+      />
+
 
     </>
   )
--- osa2/puhelinluettelo/src/Components/Persons.js
+++ osa2/puhelinluettelo/src/Components/Persons.js
@@ -1,14 +1,30 @@
 import React from 'react'
 
-const Persons = ({ persons, newSearch })  => {
+const Person = ({ person, toggleDel }) => {
+    const label = 'Delete'
+  
+    console.log('person',person)
+    return (
+      <li>
+        {person.name}
+        {person.number}
+        <button onClick={toggleDel}>{label}</button>
+      </li>
+    )
+  }
+
+
+const Persons = ({ persons, newSearch, toggleDel })  => {
 
     return (
         <ul>
             {persons.filter(person => person.name.startsWith(newSearch)).map(person => (
-            <li key={person.name}> {person.name}  {person.number} </li> ))}
-        </ul> 
-  
-    ) 
-  }
-  
+                <Person key={person.id} 
+                        person={person} 
+                        toggleDel={() => toggleDel(person={person})}
+                />))}
+        </ul>
+        )
+    }
+
   export default Persons
