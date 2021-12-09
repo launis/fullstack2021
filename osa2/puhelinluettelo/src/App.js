f2b033101376639fa07b0fcdfe17268a9e43af85
launis <62068629+launis@users.noreply.github.com>
2021-12-09 15:31:17 +0200

    2 .18

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect  } from 'react'
 
+import './index.css'
 import Header from "./Components/Header"
 import PersonForm from "./Components/PersonForm"
 import Filter from "./Components/Filter"
 import Persons from "./Components/Persons"
+import Notification from "./Components/ErrorMessage"
 import personService from "./Services/PersonsServices"
 
 
 const App = () => {
   const [persons, setPersons] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
+
   
   useEffect(() => {
     personService
@@ -40,10 +44,11 @@ const App = () => {
   const addPerson = event => {
     event.preventDefault()
 
+    const person = persons.find((person) => person.name === newName)
+    console.log('person', person, newName)
     const personObject = {name: newName, number: newNumber}
-    if (persons.some((p) => p.name === newName)) 
-      {
-      const person = persons.filter((p) => p.name === newName)[0]
+    if (person) 
+    {
       if (window.confirm(`${newName} is already added to phonebook, replase with new number?`)) 
         {
           const updatedPerson = { ...person, number: newNumber }
@@ -51,32 +56,69 @@ const App = () => {
             .update(person.id, updatedPerson)
             .then((returnedPerson) => {
               setPersons(persons.filter((p) => p.id !== person.id).concat(returnedPerson))
-              setNewName('')
-              setNewNumber('')
-            })}
-      }
+
+              setErrorMessage({
+                text: `Edited ${returnedPerson.name}`,
+                type: "success",
+              })
+              
+              setTimeout(() => {
+                setErrorMessage(null)
+              }, 3000)
+
+            })
+            .catch(error => {
+              setPersons(persons.filter(p => p.id !== person.id))
+              setErrorMessage({
+                text: error,
+                type: "error"
+              })
+              
+              setTimeout(() => {
+                setErrorMessage(null)
+              }, 3000)
+                                                                                                                                                                                                                                                                                                           })}
+    }
     else
     {
       personService
         .create(personObject)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
-          setNewName('')
-          setNewNumber('')
+
+          setErrorMessage({
+            text: `New ${returnedPerson.name}`,
+            type: "success",
+          })
+          
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 3000)
+
+        })
+        .catch(error => {
+          setErrorMessage({
+            text: error,
+            type: "error"
+          })
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 3000)
+
         })}
+  setNewName('')
+    setNewNumber('')
   }
 
 return (
     <>
+      <Notification message={errorMessage} />
       <Header header={'Phonebook'} />
-
       <Filter
         onChange={handleSearchChange}
         newSearch={newSearch}
-      />
-      
+      />      
       <Header header={'Add new'} />
-
       <PersonForm
         newName={newName}
         newNumber={newNumber}
