f5b28a299e29d6774d1ae0b016afbdcf9b7ea640
launis <62068629+launis@users.noreply.github.com>
2021-12-12 21:58:06 +0200

    Update App.js

--- osa3/puhelinluettelo/src/App.js
+++ osa3/puhelinluettelo/src/App.js
@@ -6,10 +6,8 @@ import PersonForm from "./Components/PersonForm"
 import Filter from "./Components/Filter"
 import Persons from "./Components/Persons"
 import Notification from "./Components/ErrorMessage"
-import Errors from "./Components/ErrorMessage"
 import personService from "./Services/PersonsServices"
 
-// app delete
 const App = () => {
   const [persons, setPersons] = useState([])
   const [errorMessage, setErrorMessage] = useState('')
