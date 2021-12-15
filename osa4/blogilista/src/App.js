03c65251ff9a7ef13fb940a10d94ed1dc7e68964
launis <62068629+launis@users.noreply.github.com>
2021-12-16 00:03:07 +0200

    First working version 4.2

--- osa4/blogilista/src/App.js
+++ osa4/blogilista/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import express from 'express'
+import cors from 'cors'
+import config from './utils/config.js'
+import logger from './utils/logger.js'
+import middleware from './utils/middleware.js'
+import mongoose from 'mongoose'
+import blogRouter from  './controllers/blog.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const app = express()
 
-export default App;
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+logger.info('connecting to', config.MONGODB_URI)
+
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+export default app
--- osa4/blogilista/src/index.js
+++ osa4/blogilista/src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import app from './app.js'
+import http from 'http'
+import config from './utils/config.js'
+import logger from './utils/logger.js'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const server = http.createServer(app)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
