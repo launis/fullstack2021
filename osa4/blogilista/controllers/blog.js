d571c2c59839bb96e8ca10585ba6600001826ba4
launis <62068629+launis@users.noreply.github.com>
2021-12-26 12:02:54 +0200

    minor

--- osa4/blogilista/controllers/blog.js
+++ osa4/blogilista/controllers/blog.js
@@ -54,17 +54,13 @@ router.post('/', async (request, response, next) => {
         })
 
         const savedBlog = await blog.save()
-
-        User.findByIdAndUpdate(request.user,
+        await User.findByIdAndUpdate(request.user,
             { '$push': {'blogs': savedBlog._id } },
             { 'new': true, 'upsert': true },
             function (error) {
                 if (error) throw error
             }
         )
-
-
-
         response.json(savedBlog.toJSON())
     } catch (exception) {
         next(exception)
@@ -77,16 +73,16 @@ router.delete('/:id', async (request, response, next) => {
         const user = await User.findById(request.user)
 
         const blogs = []
-        for (const [key, value] of Object.entries(user.blogs)) {
+        for await (const [key, value] of Object.entries(user.blogs)) {
             if (value != request.params.id)
             {blogs.push(value)}
         }
         if ( blog.user.toString() === request.user )
         {await Blog.findByIdAndRemove(request.params.id)
             response.status(204).end()}
-        const userObj = {blogs : blogs}
+
         await User.findOneAndUpdate(request.params.id, 
-            { $set: userObj }, { upsert: true, new: true })
+            { $set: {blogs : blogs} }, { upsert: true, new: true })
     } catch (exception) {
         next(exception)
     }
@@ -96,24 +92,19 @@ router.delete('/:id', async (request, response, next) => {
 router.put('/:id', async (request, response, next) => {
     try {
         const body = request.body
-        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+        const decodedToken = await jwt.verify(request.token, process.env.SECRET)
         if (!request.token || !decodedToken || !decodedToken.id) {
             return response.status(401).json({ error: 'token missing or invalid' })
         }
-        const blog = {
-            title: body.title,
-            author: body.author,
-            url: body.url,
-            likes: body.likes,
-            user: body.user
+        if (body.user) {
+            const user = await User.findById(body.user)
+            if (!user) {
+                return response.status(401).json({ error: 'invalid user' })
+            }
         }
 
-        const user = await User.findById(body.user)
-        if (!user) {
-            return response.status(401).json({ error: 'invalid user' })
-        }
-
-        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        const updatedBlog = await Blog.findOneAndUpdate(request.params.id, 
+            { $set: body }, { upsert: true, new: true })
         response.json(updatedBlog.toJSON())
     } catch (exception) {
         next(exception)
